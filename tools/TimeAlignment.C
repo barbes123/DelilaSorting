795c63861f03ba6d4da1f0127c425dbe49821953
yes.coucou@gmail.com <yes.coucou@gmail.com>
2021-12-10 14:55:53 +0200

    corrected started domain: now is 0 (was 1)

--- tools/TimeAlignment.C
+++ tools/TimeAlignment.C
@@ -37,7 +37,7 @@ void TimeAlignement(TH2 *matrix, float alignment_pos=0.)
     std::cout<<"max_value "<<max_value<<"\n";
     c1->WaitPrimitive();
     std::cout << "   Offset for coincID " << j+channel_offset << "  " << gaus->GetParameter(1) << std::endl;;
-    outputFile << j+channel_offset << "  " << gaus->GetParameter(1) <<"\n";;
+    outputFile << j+channel_offset-1 << "  " << gaus->GetParameter(1) <<"\n";;
     proj_y->Reset();
    }
     outputFile.close();
