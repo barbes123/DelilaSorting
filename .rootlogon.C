fe34391e627ef419e99b282a9e945ebb9b70b93b
barbes123 <yes.coucou@gmail.com>
2022-05-11 10:23:14 +0300

    .rootlogon.C

---
+++ .rootlogon.C
@@ -0,0 +1,7 @@
+{
+  TString home_path = gSystem->GetFromPipe("echo $HOME");
+  gROOT->ProcessLine(Form(".L %s/DelilaSorting/lib/libDelilaEvent.so-m64",home_path.Data()));
+  gROOT->ProcessLine(Form(".L %s/DelilaSorting/lib/libLaBrTreeEvent.so-m64",home_path.Data()));
+  gROOT->ProcessLine(Form(".L %s/DelilaSorting/lib/libElissaTreeEvent.so-m64",home_path.Data()));
+   
+  }
