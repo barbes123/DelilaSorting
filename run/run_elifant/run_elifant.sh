c5c7f570513332309e42ad57b60773085c765a93
barbes123 <yes.coucou@gmail.com>
2022-05-13 10:40:26 +0300

    Updated run folder

--- run/run_elifant/run_elifant.sh
+++ run/run_elifant/run_elifant.sh
@@ -12,6 +12,25 @@ numberofevents=${4:-0}
 
 echo "Put Parameters: run_nbr; volume_from; volume_to; number of events"
 
+lut_path="$HOME/onlineAnalysis/LookUpTables/2022_w10_paul/"
+lut_link="$HOME/DelilaSorting/"
+
+
+lut_file="LUT_DELILA_Co_Eu_1191_1195_1196.dat"
+lut_conf="coinc_gates_test_new.dat"
+lut_ta="TimeCalib_run_1195.dat"
+
+echo "$lut_path$lut_file" "$lut_link""LUT_DELILA.dat"
+
+unlink "$lut_link""LUT_DELILA.dat"
+ln -s "$lut_path$lut_file" "$lut_link""LUT_DELILA.dat"
+
+unlink "$lut_link""LUT_CONF.dat"
+ln -s "$lut_path$lut_conf" "$lut_link""LUT_CONF.dat"
+
+unlink "$lut_link""LUT_TA.dat"
+ln -s "$lut_path$lut_ta" "$lut_link""LUT_TA.dat"
+
 runnb=$FIRSTrun
 
 volnb=$volume1
