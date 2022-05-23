111cf91026e425b9063fd35fc676d808c54091de
Your Name <you@example.com>
2022-05-23 09:28:42 +0300

    added mDelila to Elissa

--- DelilaSelectorElifant.C
+++ DelilaSelectorElifant.C
@@ -1571,6 +1571,8 @@ void DelilaSelectorElifant::TreatElissaSingle()
     hAmax->Fill(DelilaEvent_.Amax);
     mAmaxEnergy->Fill(DelilaEvent_.Energy_kev,DelilaEvent_.Amax);
     hDelila0[DelilaEvent_.det_def]->Fill(DelilaEvent_.Energy_kev); 
+    mDelila->Fill(domain,DelilaEvent_.Energy_kev);
+
     
     if (blFillAmaxEnergyDom) mAmaxEnergyDom[DelilaEvent_.domain]->Fill(DelilaEvent_.Energy_kev,DelilaEvent_.Amax);
     
